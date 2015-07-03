6327147fa9215c86b78857e10d824de9e6b7882f
JackSpirou <jack.spirou@me.com>
2015-07-03 16:00:01 -0500

    updating source files

--- publish.sh
+++ publish.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+hugo
+
+git add .
+git commit -m "updating source files"
+git push origin master
+
 cd public
 git add .
-git commit -m "site update"
+git commit -m "updating static files"
 git push origin gh-pages
